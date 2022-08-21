6c27a19ad4c95c84b8aa89fa724373c588a4def5
H4wk <quasand89@gmail.com>
2022-08-21 22:12:25 +0200

    refactor v2

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
-import json
-import math
 import curses
 from curses import wrapper
 import curses.ascii
-from random import sample
-import time
+from time import time
 import os
-from util import (is_backspace, is_arrow, is_resize, is_ignored_key, is_enter,
-                  is_tab, is_escape, is_ctrl_r)
+from util import (
+    calculate_accuracy,
+    is_backspace,
+    is_arrow,
+    is_resize,
+    is_ignored_key,
+    is_enter,
+    is_tab,
+    is_escape,
+    is_ctrl_r,
+    fill_spaces,
+    load_random_text,
+    get_number_of_lines,
+    word_wrap,
+    calculate_cpm,
+    calculate_wpm,
+)
 
 
 class Typer:
     """Class for enclosing all things required to run the app."""
 
     def __init__(self):
-        self.number_of_words = 20
-        self.text = self.load_random_text()
+        """Initialize the typer class."""
+        self.number_of_words = 2
+        self.text = load_random_text(self.number_of_words)
         self.current = []
+        self.mistyped_chars = 0
+        self.started = False
 
         self.wpm = 0
         self.cpm = 0
         self.accuracy = 0
+
+        self.win_height = 0
+        self.win_width = 0
+        self.lines = 0
+
+        self.color = None
 
         os.environ.setdefault("ESCDELAY", "0")
         wrapper(self.main)
@@ -28,18 +49,16 @@ class Typer:
     def main(self, win):
         self.initialize(win)
 
-        # accuracy = 0
-        # consistency = 0
-
         while True:
-            choice = self.play(win)
+            choice = self.start_typing_test(win)
             if not is_tab(choice):
                 break
 
     def initialize(self, win):
+        """Initialize initial state of the curses interface."""
         self.win_height, self.win_width = win.getmaxyx()
-        self.text = self.word_wrap()
-        self.lines = self.get_number_of_lines()
+        self.text = word_wrap(self.text, self.win_width)
+        self.lines = get_number_of_lines(self.text, self.win_width)
 
         curses.init_color(1, 1000, 1000, 1000)
         curses.init_color(2, 1000, 0, 0)
@@ -49,6 +68,7 @@ class Typer:
 
         class Color:
             """Helper Color class (something like enum)"""
+
             WHITE_BOLD = curses.color_pair(1) | curses.A_BOLD
             WHITE_DIM = curses.color_pair(1) | curses.A_DIM
             RED = curses.color_pair(2)
@@ -58,159 +78,92 @@ class Typer:
         win.nodelay(True)
         win.timeout(100)
 
-    def play(self, win) -> str:
-        width = win.getmaxyx()[1]
-        text = self.load_random_text()
-        lines = self.get_number_of_lines()
-        text = self.word_wrap()
-
-        self.start_typing_test(win, lines)
-
-        win.addstr(lines + 5, 0, "You've completed the text.\n")
-        win.addstr(lines + 6, 0, "TAB to play again.\n")
-
-        choice = win.getkey()
-        return choice
-
-    def load_random_text(self) -> str:
-        """Load random text from json file."""
-        with open("words.json") as f:
-            dat = json.load(f)
-
-        wordlist = dat["words"]
-        words = sample(wordlist, self.number_of_words)
-        return " ".join(words)
-
-    def get_number_of_lines(self) -> int:
-        """return number of lines that fit on screen"""
-        return int(math.ceil(len(self.text) / self.win_width))
-
-    def word_wrap(self) -> str:
-        """Wrap text on the screen according to the window width.
-        Returns text with extra spaces which makes the string word wrap.
-        """
-        for line in range(1, self.get_number_of_lines() + 1):
-            if line * self.win_width >= len(self.text):
-                continue
-
-            index = line * self.win_width - 1
-            if self.text[index] == " ":
-                continue
-
-            index = self.text[:index].rfind(" ")
-            space_count = line * self.win_width - index
-            space_string = " " * space_count
-            self.text = self.text[:index] + space_string + self.text[index +
-                                                                     1:]
-
-        return self.text
-
-    def calculate_cpm(self, start_time: float) -> float:
-        time_taken = (time.time() - start_time) / 60
-        return round(len(self.current) / time_taken, 1)
-
-    def calculate_wpm(self, words: list["str"], start_time: float) -> float:
-        """Return typing speed in words per minute
-
-        Args:
-            words: List of words from sample text.
-            start_time: The time when user starts typing the text in seconds.
-
-        Return:
-            Speed in words per minute.
-        """
-
-        time_taken = (time.time() - start_time) / 60
-        return round(len(words) / time_taken, 1)
-
-    def calculate_accuracy(chars_typed: int, wrongly_typed: int) -> float:
-        """Return accuracy as % between 0 and 100"""
-        correctly_typed = chars_typed - wrongly_typed
-        return round(correctly_typed / max(chars_typed, 1) * 100, 1)
-
-    def fill_spaces(self, idx: int):
-        while idx < len(self.text) and self.text[idx] == " ":
-            self.current.append(" ")
-            idx += 1
-        return idx
-
-    def display_text(
-        self,
-        stdscr,
-        lines: int,
-        start_time: float,
-        has_started: bool,
-    ) -> None:
+    def display_text(self, win, start_time: float) -> None:
         """Display target text, WPM, CPM, time elapsed since the beginning and
         the text that the user is currently writing."""
-        stdscr.addstr(self.text, self.color.WHITE_BOLD)
-        stdscr.addstr(lines + 1, 0, "esc quit")
-        if not has_started:
-            stdscr.addstr(lines + 1, 8, " | ctrl+r generate new text.")
-        stdscr.addstr(lines + 2, 0, f"CPM: {self.cpm}")
-        stdscr.addstr(lines + 3, 0, f"WPM: {self.wpm}")
-        stdscr.addstr(lines + 4, 0,
-                      f"time: {round(time.time() - start_time, 2)}s")
-        stdscr.move(0, 0)
+        win.addstr(self.text, self.color.WHITE_BOLD)
+        win.addstr(self.lines + 1, 0, "esc quit")
+        if not self.started:
+            win.addstr(self.lines + 1, 8, " | ctrl+r generate new text.")
+        win.addstr(self.lines + 2, 0, f"CPM: {self.cpm}")
+        win.addstr(self.lines + 3, 0, f"WPM: {self.wpm}")
+        win.addstr(self.lines + 4, 0, f"accuracy: {self.accuracy}")
+        win.addstr(self.lines + 5, 0, f"time: {round(time() - start_time, 2)}s")
+        win.move(0, 0)
 
-        width = stdscr.getmaxyx()[1]
+        width = win.getmaxyx()[1]
         line = 0
         for i in range(len(self.current)):
             if self.current[i] == self.text[i]:
-                stdscr.addstr(line, i % width, self.text[i],
-                              self.color.WHITE_DIM)
+                win.addstr(line, i % width, self.text[i], self.color.WHITE_DIM)
             else:
-                stdscr.addstr(line, i % width, self.text[i], self.color.RED)
+                win.addstr(line, i % width, self.text[i], self.color.RED)
+                self.mistyped_chars += 1
 
             if i % width == width - 1:
                 line += 1
 
-    def start_typing_test(self, stdscr, lines: int) -> None:
-        start_time = time.time()
-        has_started = False
+    def start_typing_test(self, win) -> None:
+        self.text = load_random_text(self.number_of_words)
+        self.text = word_wrap(self.text, self.win_width)
+        self.lines = get_number_of_lines(self.text, self.win_width)
+        self.current = []
+        self.started = False
+        win.nodelay(True)
+        win.timeout(100)
+
+        start_time = time()
         while True:
             if self.current == []:
-                start_time = time.time()
+                start_time = time()
 
-            self.cpm = self.calculate_cpm(start_time)
-            self.wpm = self.calculate_wpm("".join(self.current).split(),
-                                          start_time)
+            self.cpm = calculate_cpm(self.current, start_time)
+            self.wpm = calculate_wpm("".join(self.current).split(), start_time)
+            # self.accuracy = calculate_accuracy(len(self.current), self.mistyped_chars)
 
-            stdscr.erase()  # <- fixes flicker
-            self.display_text(stdscr, lines, start_time, has_started)
-            stdscr.refresh()
+            win.erase()  # <- fixes flicker
+            self.display_text(win, start_time)
+            win.refresh()
 
             if "".join(self.current) == self.text:
-                stdscr.nodelay(False)
-                break
+                win.addstr(self.lines + 5, 0, "You've completed the text.\n")
+                win.addstr(self.lines + 6, 0, "TAB to play again.\n")
+
+                win.nodelay(False)
+                choice = win.get_wch()
+                return choice
 
             try:
-                c = stdscr.getkey()
+                c = win.get_wch()
             except:
                 continue
 
-            if is_ctrl_r(c) and not has_started:
-                width = stdscr.getmaxyx()[1]
-                self.text = self.load_random_text()
-                lines = self.get_number_of_lines()
-                self.text = self.word_wrap()
-
-            elif (is_arrow(c) or is_resize(c) or is_ignored_key(c)
-                  or is_enter(c) or is_tab(c)):
-                continue
+            if is_ctrl_r(c) and not self.started:
+                self.text = load_random_text(self.number_of_words)
+                self.lines = get_number_of_lines(self.text, self.win_width)
+                self.text = word_wrap(self.text, self.win_width)
 
             elif is_backspace(c):
                 if len(self.current) > 0:
                     self.current.pop()
 
+            elif (
+                is_arrow(c)
+                or is_resize(c)
+                or is_ignored_key(c)
+                or is_enter(c)
+                or is_tab(c)
+            ):
+                continue
+
             elif is_escape(c):
-                stdscr.nodelay(False)
+                win.nodelay(False)
                 break
 
             elif c == " ":
-                self.fill_spaces(len(self.current))
+                fill_spaces(len(self.current), self.current, self.text)
 
             elif len(self.current) < len(self.text):
                 self.current.append(c)
-                if not has_started:
-                    has_started = True
+                if not self.started:
+                    self.started = True
--- util.py
+++ util.py
@@ -1,9 +1,12 @@
 import curses
+import json
+from random import sample
+from math import ceil
+from time import time
 
 
 def is_backspace(key: str) -> bool:
-    return key in ("KEY_BACKSPACE", "\b", "\x7f", curses.KEY_BACKSPACE,
-                   curses.KEY_DC)
+    return key in ("KEY_BACKSPACE", "\b", "\x7f", curses.KEY_BACKSPACE, curses.KEY_DC)
 
 
 def is_arrow(key: str) -> bool:
@@ -27,8 +30,79 @@ def is_tab(key: str) -> bool:
 
 
 def is_escape(key: str) -> bool:
-    return ord(key) == curses.ascii.ESC
+    if isinstance(key, str) and len(key) == 1:
+        return ord(key) == curses.ascii.ESC
+    return False
 
 
 def is_ctrl_r(key: str) -> bool:
     return key == "\x12"
+
+
+def load_random_text(number_of_words: int) -> str:
+    """Load random text from json file."""
+    with open("words.json") as f:
+        dat = json.load(f)
+
+    wordlist = dat["words"]
+    words = sample(wordlist, number_of_words)
+    return " ".join(words)
+
+
+def fill_spaces(idx: int, current: list["str"], text: str) -> int:
+    """Fill current with spaces so that amount of spaces is matched with text"""
+    while idx < len(text) and text[idx] == " ":
+        current.append(" ")
+        idx += 1
+    return idx
+
+
+def get_number_of_lines(text: str, win_width: int) -> int:
+    """return number of lines that fit on screen"""
+    return int(ceil(len(text) / win_width))
+
+
+def word_wrap(text: str, win_width: int) -> str:
+    """Wrap text on the screen according to the window width.
+    Returns text with extra spaces which makes the string word wrap.
+    """
+    for line in range(1, get_number_of_lines(text, win_width) + 1):
+        if line * win_width >= len(text):
+            continue
+
+        index = line * win_width - 1
+        if text[index] == " ":
+            continue
+
+        index = text[:index].rfind(" ")
+        space_count = line * win_width - index
+        space_string = " " * space_count
+        text = text[:index] + space_string + text[index + 1 :]
+
+    return text
+
+
+def calculate_cpm(current: list["str"], start_time: float) -> float:
+    time_taken = (time() - start_time) / 60
+    return round(len(current) / time_taken, 1)
+
+
+def calculate_wpm(words: list["str"], start_time: float) -> float:
+    """Return typing speed in words per minute
+
+    Args:
+        words: List of words from sample text.
+        start_time: The time when user starts typing the text in seconds.
+
+    Return:
+        Speed in words per minute.
+    """
+
+    time_taken = (time() - start_time) / 60
+    return round(len(words) / time_taken, 1)
+
+
+def calculate_accuracy(chars_typed: int, wrongly_typed: int) -> float:
+    """Return accuracy as a % between 0 and 100"""
+    correctly_typed = chars_typed - wrongly_typed
+    return round(correctly_typed / max(chars_typed, 1) * 100, 1)
